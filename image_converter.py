892ed0e06993214a5d95dfca510b2f5e103da0fd
Mani <nman@stevens.edu>
2018-03-28 18:37:38 -0400

    update and test

--- image_converter.py
+++ image_converter.py
@@ -4,12 +4,13 @@ from PIL import Image
 
 
 def convert_rgb(img_name='b399f09dee9c3c67.jpeg'):
-    img=Image.open(img_name)
+    img=Image.open('./train/'+img_name)
     image_rgb = img.convert("RGB")    #convert image into RGB pixel
     #image_bw = img.convert("L")# ---- B/L pixel
     # convert image to a matrix with values from 0 to 255 (uint8)
-    rgb_arr = np.asarray(image_gr)
+    rgb_arr = np.asarray(image_rgb)
     print("After conversion to numerical representation: \n\n %r" % rgb_arr)
-    %matplotlib inline
 
-convert_rgb(img_name)
+    return rgb_arr
+
+convert_rgb(img_name='b399f09dee9c3c67.jpeg')
