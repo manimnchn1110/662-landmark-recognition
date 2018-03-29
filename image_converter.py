ec17ed45254c5a510d9290eff4ec89d3b5ac9de4
Mani <nman@stevens.edu>
2018-03-28 20:08:29 -0400

    update

--- image_converter.py
+++ image_converter.py
@@ -8,15 +8,17 @@ def convert_rgb(img_name):
     # convert image to a matrix with values from 0 to 255 (uint8)
     rgb_arr = np.asarray(image_rgb)
     return rgb_arr
+#Example:
+#rgb_arr = convert_rgb('b399f09dee9c3c67.jpeg')
+#print(rgb_arr)
 
-
-def get_x_shape(img_name='b399f09dee9c3c67.jpeg'):
+def get_x_shape(img_name):
     rgb_arr = convert_rgb(img_name)
     shape = []
     shape.append(None)
     for x in rgb_arr.shape:
         shape.append(x)
     return shape
-
-shape = get_x_shape('b399f09dee9c3c67.jpeg')
-print(shape)
+#Example:
+#shape = get_x_shape('b399f09dee9c3c67.jpeg')
+#print(shape)
