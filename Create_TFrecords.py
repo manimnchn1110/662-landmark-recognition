290535d4af4018a4dbe608dbf27ff7686409b556
manimnchn1110 <31875921+manimnchn1110@users.noreply.github.com>
2018-04-11 12:12:38 -0400

    Error Count

--- Create_TFrecords.py
+++ Create_TFrecords.py
@@ -8,15 +8,25 @@ df = pd.read_csv('train.csv')
 # First_100_training_set
 folder_path = './train' + '/'
 writer = tf.python_io.TFRecordWriter("Landmark_train.tfrecords")
-for i in range(100):
-    img_path = folder_path + df.id[i] + '.jpeg'
-    output = int(df.landmark_id[i])
-    img = Image.open(img_path)
-    img_raw = img.tobytes()
-    example = tf.train.Example(features=tf.train.Features(feature={
-        "label": tf.train.Feature(int64_list=tf.train.Int64List(value=[output])),
-        'img_raw': tf.train.Feature(bytes_list=tf.train.BytesList(value=[img_raw]))
-    }))
-    writer.write(example.SerializeToString())
+error = 0
+error_li = []
+for i in range(0,10000):
+    try:
+        img_path = folder_path + df.id[i] + '.jpeg'
+        img = Image.open(img_path)
+        output = int(df.landmark_id[i])
+        img_raw = img.tobytes()
+        example = tf.train.Example(features=tf.train.Features(feature={
+            "label": tf.train.Feature(int64_list=tf.train.Int64List(value=[output])),
+            'img_raw': tf.train.Feature(bytes_list=tf.train.BytesList(value=[img_raw]))
+        }))
+        writer.write(example.SerializeToString())
+    except:
+        print('img not exists!')
+        error_li.append(str(df.id[i]))
+        error += 1
+
 
 writer.close()
+
+print(error, error_li)
